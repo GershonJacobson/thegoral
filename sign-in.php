2f63fe2919baf0b7caea662be58b095f7daf0a32
GBJuicy <gbjacobson05@gmail.com>
2025-09-17 16:07:12 -0400

    edit

--- sign-in.php
+++ sign-in.php
@@ -1,60 +1,65 @@
 <?php
-error_reporting(0);
-session_start();
+// sign-in.php (Upgraded Version)
+
+declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set('display_errors', '1'); // Good for development, turn off for production
 
 require("config/session.php");
 
-if($getUserID != "") {
-	header("Location: /");
-	die();
+// If the user is already logged in, redirect them to the homepage.
+if(!empty($getUserID)) {
+    header("Location: /");
+    exit(); // Always exit after a redirect
 }
+
+/**
+ * Generates and stores a CSRF token in the session if one doesn't exist.
+ * @return string The CSRF token.
+ */
+function getCsrfToken(): string {
+    if (empty($_SESSION['csrf_token'])) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['csrf_token'];
+}
+
+$csrfToken = getCsrfToken();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-	<meta name="robots" content="index, follow">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="robots" content="index, follow">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <title>Sign in - The Goral</title>
     <link rel="icon" type="image/x-icon" href="assets/images/favicon.svg" />
 
-    <link
-      rel="stylesheet"
-      href="assets/css/bootstrap/css/bootstrap.min.css"
-    />
+    <link rel="stylesheet" href="assets/css/bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" href="assets/css/style.css" />
-	<link rel="stylesheet" href="assets/css/sweetalert.css" />
+    <link rel="stylesheet" href="assets/css/sweetalert.css" />
     <link rel="stylesheet" href="assets/font/fontawesome/css/all.min.css" />
 
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<script src="assets/js/sweetalert.min.js"></script>
-    <script src="assets/js/sign-in.js"></script>
-    <script src="assets/font/fontawesome/js/all.min.js"></script>
+    <script src="assets/js/jquery.min.js" defer></script>
+    <script src="assets/js/bootstrap/js/bootstrap.bundle.min.js" defer></script>
+    <script src="assets/js/sweetalert.min.js" defer></script>
+    <script src="assets/js/sign-in.js" defer></script>
+    <script src="assets/font/fontawesome/js/all.min.js" defer></script>
   </head>
   <body style="background: #fff">
     <div class="login-container">
       <div class="signup-logo">
-        <a href="/"
-          ><img src="assets/images/logo-dark.svg" alt=""
-        /></a>
+        <a href="/"><img src="assets/images/logo-dark.svg" alt="The Goral Logo" /></a>
       </div>
       <div class="signup-menu">
         <div class="row">
           <div class="col-md col-50">
-            <a
-              class="signup"
-              href="sign-up"
-              >Create your account</a
-            >
+            <a class="signup" href="sign-up">Create your account</a>
           </div>
           <div class="col-md col-50">
-            <a
-              class="signin menu-active"
-              href="sign-in"
-              >Login</a
-            >
+            <a class="signin menu-active" href="sign-in">Login</a>
           </div>
         </div>
       </div>
@@ -71,31 +76,34 @@ if($getUserID != "") {
             <a href="sign-up">Create an Account</a>
           </div>
         </div>
-        <div class="row">
-          <div class="col-md">
-            <input type="email" placeholder="you@example.com" id="email" />
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md">
-            <input type="password" placeholder="Password" id="password" />
-          </div>
-        </div>
-        <div class="row">
-          <div class="rm" style="margin-top: 3px; align-items: center;">
-            <div><input type="checkbox" name="remember-me" id="remember-me" /></div>
-           
-			<div style="padding-left: 5px; font-size: 12px;"><label for="remember-me"> Remember me</label></div>
-          </div>
-        </div>
-        <div class="row">
-          <button type="submit" class="btnSignup" id="btnLogin">Log in</button>
-        </div>
+        
+        <form id="loginForm" method="POST">
+            <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($csrfToken) ?>">
+            <div class="row">
+              <div class="col-md">
+                  <label for="email" class="form-label visually-hidden">Email Address</label>
+                  <input type="email" class="form-control" name="emailAddress" placeholder="you@example.com" id="email" autocomplete="email" required />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md">
+                  <label for="password" class="form-label visually-hidden">Password</label>
+                  <input type="password" class="form-control" name="password" placeholder="Password" id="password" autocomplete="current-password" required />
+              </div>
+            </div>
+            <div class="row">
+              <div class="rm" style="margin-top: 3px; align-items: center;">
+                <div><input type="checkbox" name="remember-me" id="remember-me" /></div>
+                <div style="padding-left: 5px; font-size: 12px;"><label for="remember-me"> Remember me</label></div>
+              </div>
+            </div>
+            <div class="row">
+              <button type="submit" class="btnSignup" id="btnLogin">Log in</button>
+            </div>
+        </form>
         <div class="row">
           <div class="forgot">
-            <a href="forgot-password"
-              >Forgot Password</a
-            >
+            <a href="forgot-password">Forgot Password</a>
           </div>
         </div>
       </div>
@@ -104,7 +112,7 @@ if($getUserID != "") {
       <div class="container">
         <div class="row">
           <div class="social-media text-center">
-            <span><img src="assets/images/logo.svg" alt="" /></span>
+            <span><img src="assets/images/logo.svg" alt="The Goral Logo" /></span>
             <i class="fa-brands fa-facebook-f"></i>
             <i class="fa-brands fa-twitter"></i>
             <i class="fa-brands fa-linkedin-in"></i>
@@ -113,9 +121,7 @@ if($getUserID != "") {
         </div>
         <div class="row">
           <div class="menu-footer">
-            <a href="http://thegoral.com">Home</a>
-            <a href="live-campaign.html">Live Campaigns</a>
-            <a href="all-campaign.html">All Campaigns</a>
+            <a href="/">Home</a>
           </div>
         </div>
         <div class="row">
@@ -127,7 +133,7 @@ if($getUserID != "") {
           </div>
         </div>
         <div class="row">
-          <div class="copyright">© 2022 The Goral</div>
+          <div class="copyright">© <?= date('Y') ?> The Goral</div>
         </div>
       </div>
     </div>
