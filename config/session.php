1b5875bc3763b950ed7386e06eef902a2f95fbaf
GBJuicy <gbjacobson05@gmail.com>
2025-09-25 19:09:18 -0400

    Update session.php

--- config/session.php
+++ config/session.php
@@ -1,18 +1,10 @@
 <?php
-require("db.php");
+// config/session.php (Corrected)
+declare(strict_types=1);
 
-if(!empty($_SESSION['userGoral']) || !empty($_COOKIE['cookielogin']['user'])) {
-	$userSession = $_SESSION['userGoral'];
-	$userCookie = $_COOKIE['cookielogin']['user'];
-
-	$qSession = mysqli_query($con, "SELECT * FROM tbl_users WHERE user_id = '" . $userSession . "' OR user_id = '" . $userCookie . "'");
-	$dSession = mysqli_fetch_array($qSession);
-	
-	$getUserID = $dSession['user_id'];
-	$getFirstName = $dSession['first_name'];
-	$getLastName = $dSession['last_name'];
-	$getEmailAddress = $dSession['email_address'];
-	$getPhone = $dSession['phone'];
-	$getUserRole = $dSession['admin'];
+// Start the session.
+// session_start() must be the very first thing in your PHP document.
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
 }
 ?>
